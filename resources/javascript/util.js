6604843b288bfd542eb0f76da6a2ce7622b3c528
Blake Patterson <brpatterson00@gmail.com>
2021-04-04 21:02:59 -0400

    Created utility file and wrote embedVideo function

---
+++ resources/javascript/util.js
@@ -0,0 +1,16 @@
+//This file contains a single util object that can be used for basic utility functions
+
+var util = {
+
+    //This function takes a youtube embed url (which can be obtained by right clicking a youtube video and selecting the embed option) 
+    //and returns a div containing the embedded video
+    embedVideo: function (url) {
+        return `<iframe width="640" height="360" 
+                  src="`+ url +` " 
+                  title="Genki Study Resources" 
+                  frameborder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen>
+                </iframe>`;
+      }
+};
