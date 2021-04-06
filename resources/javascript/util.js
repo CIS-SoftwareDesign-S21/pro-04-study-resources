d76d1773546ad42ed1d387896ef97ef5afc87221
KevlarBoxers <franklinlantigua90@gmail.com>
2021-04-06 11:11:52 -0400

    -stylesheet.css has a frame style
    -tried to add the style in util.js

--- resources/javascript/util.js
+++ resources/javascript/util.js
@@ -5,13 +5,15 @@ var util = {
     //This function takes a youtube embed url (which can be obtained by right clicking a youtube video and selecting the embed option) 
     //and returns a div containing the embedded video
     embedVideo: function (url) {
-        return `<iframe width="960" height="540" 
-                  src="`+ url +` " 
+        return `
+                <iframe 
+                  class = "frame"
+                  src="` + url + ` " 
                   title="Genki Study Resources" 
                   frameborder="0" 
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                   allowfullscreen>
                 </iframe>`;
-      }
+    }
 
 };
