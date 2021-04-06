608c6a6d1331cc72d223a36b4e9213c2afd2028d
Yin, Jiayi <yinjiayi5@gmail.com>
2021-04-06 13:02:11 -0400

    Added button or sub/nosub; Added new JS util function

--- resources/javascript/util.js
+++ resources/javascript/util.js
@@ -12,6 +12,26 @@ var util = {
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                   allowfullscreen>
                 </iframe>`;
-      }
+      },
 
+      //Temporary: This function will create buttons for all video lessons.
+      subtitlesButton: function(){
+        return `<div id="button" class="box-3">
+                  <div id="sub" class="btn btn-three active">With Subtitle</div>
+                  <div id="noSub" class="btn btn-three">Without Subtitle</div>
+                </div>`;
+      },
+
+      changeVideo: function(){
+        var header = document.getElementById("button");
+          var btns = header.getElementsByClassName("btn");
+          for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function () {
+              var current = document.getElementsByClassName("active");
+              current[0].className = current[0].className.replace(" active", "");
+              this.className += " active";
+
+            });
+          }
+      }
 };
