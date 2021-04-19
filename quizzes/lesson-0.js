6c2de8c74f45d5f3728ce5b95e028c33bf64df3a
KevlarBoxers <franklinlantigua90@gmail.com>
2021-04-19 13:58:42 -0400

    -Added util.writtenQuiz utility function that puts together the html formatting for the written questions
    -Added the first quiz .js file that can be loaded and used as a template for any given video quiz
    
    -Removed the old html inside of lessons with fresh new programmatically inserted quiz questions

---
+++ quizzes/lesson-0.js
@@ -0,0 +1,96 @@
+var quiz = {
+    mc: [
+        {
+            "question" : "(Timestamp - 0:15) It's 9am and you pass by a classmate, how do you greet them?.",
+            "answers" : [
+                "Aおはよう。",
+                "こんにちは。",
+                "こんばんは。"
+            ]
+        },
+
+        {
+            "question" : "(Timestamp - 0:30) You arrive to class at 9am and see your teacher, how do you greet them?",
+            "answers" : [
+                "Aおはようございます。",
+                "おはよう。",
+                "こんにちは。"
+            ]
+        },
+
+        {
+            "question" : "(Timestamp - 0:37) After greeting the teacher, you sit down next to your friend in class. How do you greet them?",
+            "answers" : [
+                "Aおはよう。",
+                "はじめまして。",
+                "おはようございます。"
+            ]
+        },
+
+        {
+            "question" : "(Timestamp - 0:47) It's 2pm and you pass by your teacher in the hallway, how do you greet them?",
+            "answers" : [
+                "Aこんにちは。",
+                "こんにちはございます。",
+                "おはよう。"
+            ]
+        },
+
+        {
+            "question" : "(Timestamp - 1:02) It is eight o'clock at night and you pass by your teacher in the stairwell, how do you greet them?",
+            "answers" : [
+                "Aこんばんは。",
+                "こんばんはございます。",
+                "おはようございます。"
+            ]
+        },
+
+        {
+            "question" : "(Timestamp - 1:15) Class is over and you are leaving for the day, what do you say to your teacher as you leave?",
+            "answers" : [
+                "Aさようなら。",
+                "こんばんは。",
+                "おやすみ（なさい）。"
+            ]
+        },
+
+        {
+            "question" : "(Timestamp - 1:30) It's 11pm, you are tired, and so you are going to bed. What do you say to your mother before going to bed?",
+            "answers" : [
+                "Aおやすみなさい。",
+                "こんばんは。",
+                "さようなら。"
+            ]
+        }
+    ],
+    written: [
+        {
+            "question": "(Timestamp - 0:15) It's 9am and you pass by a classmate, how do you greet them?.<br>",
+            "answer": "{おはよう}。"
+        },
+        {
+            "question": "(Timestamp - 0:30) You arrive to class at 9am and see your teacher, how do you greet them?<br>",
+            "answer": "{おはようございます}。"
+        },
+        {
+            "question": "(Timestamp - 0:37) After greeting the teacher, you sit down next to your friend in class. How do you greet them?<br>",
+            "answer": "{おはよう}。"
+        },
+        {
+            "question": "(Timestamp - 0:47) It\\'s 2pm and you pass by your teacher in the hallway, how do you greet them?<br>",
+            "answer": "{こんにちは}。"
+        },
+        {
+            "question": "(Timestamp - 1:02) It is eight o\\'clock at night and you pass by your teacher in the stairwell, how do you greet them?<br>",
+            "answer": "{こんばんは}。"
+        },
+        {
+            "question": "(Timestamp - 1:15) Class is over and you are leaving for the day, what do you say to your teacher as you leave?<br>",
+            "answer": "{さようなら}。"
+        },
+        {
+            "question": "(Timestamp - 1:30) It\\'s 11pm, you are tired, and so you are going to bed. What do you say to your mother before going to bed?<br>",
+            "answer": "{おやすみなさい}。"
+        }
+    ]
+};
--- resources/javascript/util.js
+++ resources/javascript/util.js
@@ -6,44 +6,53 @@ var util = {
     //and returns a div containing the embedded video
     embedVideo: function (url) {
         return `<iframe width="960" height="540" 
-                  src="`+ url +` " 
+                  src="` + url + ` " 
                   title="Genki Study Resources" 
                   frameborder="0" 
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                   allowfullscreen>
                 </iframe>`;
-      },
+    },
+    writtenQuiz: function (questionsAndAnswers) {
+        qAndA = ""
+        for(let i = 0; i < questionsAndAnswers.length; i++){
+            qAndA += '<div class="problem">';
+            qAndA += questionsAndAnswers[i].question + questionsAndAnswers[i].answer;
+            qAndA += '</div>'
+        }
 
-      //Temporary: This function will create buttons for all video lessons.
-      subtitlesButton: function(){
+        return qAndA;
+    },
+
+    //Temporary: This function will create buttons for all video lessons.
+    subtitlesButton: function () {
         return `<div id="button" class="box-3">
                   <div id="sub" class="btn btn-three active">With Subtitle</div>
                   <div id="noSub" class="btn btn-three">Without Subtitle</div>
                 </div>`;
-      },
+    },
 
-      changeVideo: function(url1, url2){
+    changeVideo: function (url1, url2) {
         var header = document.getElementById("button");
         var btns = header.getElementsByClassName("btn");
         for (var i = 0; i < btns.length; i++) {
-          btns[i].addEventListener("click", function () {
-          var current = document.getElementsByClassName("active");
-          current[0].className = current[0].className.replace(" active", "");
-          this.className += " active";
+            btns[i].addEventListener("click", function () {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
 
-          var sub = document.getElementById("sub");
-          var unsub = document.getElementById("nosub");
-          if(sub.classList.contains('active')){
-            //console.log("Hello from console sub!");
-            var video = util.embedVideo(url1);
-            document.getElementById("video").innerHTML = video;
-          }
-          else {
-            //console.log("Hello from console unsub!")
-            var video = util.embedVideo(url2);
-            document.getElementById("video").innerHTML = video;
-          }
-          });
+                var sub = document.getElementById("sub");
+                var unsub = document.getElementById("nosub");
+                if (sub.classList.contains('active')) {
+                    //console.log("Hello from console sub!");
+                    var video = util.embedVideo(url1);
+                    document.getElementById("video").innerHTML = video;
+                } else {
+                    //console.log("Hello from console unsub!")
+                    var video = util.embedVideo(url2);
+                    document.getElementById("video").innerHTML = video;
+                }
+            });
         }
-      }
+    }
 };
